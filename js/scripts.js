95210d96e537f1782148a7da504672e14b490b77
vanyaruda <ivan.ruda98@gmail.com>
2022-12-23 19:01:09 +0200

    add chapter

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var trigger = document.getElementById('toggle');
+    var box = document.getElementById('menu');
+
+    toggle.addEventListener('click', function () {
+        box.classList.toggle('active');
+    })
+});
