0257d5c86eb28e11c0547d86badb4b26718b7661
vanyaruda <ivan.ruda98@gmail.com>
2022-12-29 15:17:06 +0200

    fix carousel

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,25 @@ $(document).ready(function () {
     toggle.addEventListener('click', function () {
         box.classList.toggle('active');
     })
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: ['', ' '],
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 2000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+
+            1000: {
+                items: 5
+            }
+        }
+    });
 });
